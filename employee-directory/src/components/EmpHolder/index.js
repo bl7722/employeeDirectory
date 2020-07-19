fe14b21e77201f8c4e148686874f8d8f44447385
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-18 22:29:05 -0500

    updated EmpHolder/idnex.js. Navbar visible.

--- employee-directory/src/components/EmpHolder/index.js
+++ employee-directory/src/components/EmpHolder/index.js
@@ -4,6 +4,45 @@ import Table from "../Table/index"
 import Header from "../Header/index";
 import API from "../../utils/API"
 
+class EmpHolder extends Component {
+    state = {
+    result: [],
+    search: ""
+  };
 
+  componentDidMount(){
+    this.emps()
+  };
+
+  emps = () => {
+    API.getEmployees()
+        .then(res => this.setState({ result: res.data.results }))
+        .catch(err => console.log(err));
+  };
+
+  
+    
+render(){
+   let epmsList;
+    epmsList = this.state.result.map(employee=>
+    <Table 
+        key = {employee.id.value}
+        firstName ={employee.name.first} 
+        lastName= {employee.name.last} 
+        picture={employee.picture.large} 
+        phone={employee.phone} 
+        email={employee.email}
+    />)
+    return(
+        <>
+        <Header
+        />
+        <Container>
+        </Container>
+        
+        </>
+    )
+}
+}
 
 export default EmpHolder;
