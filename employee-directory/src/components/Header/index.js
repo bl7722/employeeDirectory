ea8dcc3a927a60ce45770aa6ae4d6e9b0c4f690f
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-18 22:17:45 -0500

    updated Header/index.js

--- employee-directory/src/components/Header/index.js
+++ employee-directory/src/components/Header/index.js
@@ -1,5 +1,38 @@
 import React from "react";
 
-
+function Header(props) {
+  return (
+<div class="container">
+  <navbar class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a className="navbar-brand" href="#">Employee Directory</a>
+    <form className="form-inline">
+      <input 
+        className="form-control mr-sm-4" 
+        type="search" 
+        placeholder="Search by Name" 
+        onChange={props.handleInputChange} 
+        value={props.search}
+        name="search"
+      />
+  
+      <button 
+        className="btn btn-light my-2 my-sm-0" 
+        onClick={props.handleFormSubmit}
+      >
+        Search
+      </button>
+      
+      <button 
+        className="btn btn-light  ml-sm-4 my-2 my-sm-0" 
+        onClick={props.emps}
+      >
+        Reset
+      </button>
+    
+    </form>
+  </navbar>
+</div>
+  );
+}
 
 export default Header;
