4b8807a4b1ba92b066a25be908ead1202d96cefe
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-18 22:16:20 -0500

    created src/components/EmpHolder/index.js

---
+++ employee-directory/src/components/EmpHolder/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+import Container from "../Container/index";
+import Table from "../Table/index"
+import Header from "../Header/index";
+import API from "../../utils/API"
+
+
+
+export default EmpHolder;
