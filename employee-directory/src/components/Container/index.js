0fccfdd88dfa36b14070270bd1d10891ac40c100
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-18 21:56:29 -0500

    updated public/index.html and manifest.json, src/App.js and index.js, src/utils/API.js, src/components/Container/index.js

--- employee-directory/src/components/Container/index.js
+++ employee-directory/src/components/Container/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Container(props) {
     return (
-    <div className={`container${props.fluid ? "-fluid" : ""} bg-light`}>
+    <div className={`container${props.fluid ? "-fluid" : ""}`}>
       {props.children}
     </div>);
   }
 
-export default Container;
+export default Container
--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default {
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+  getEmployees: function() {
+    return axios.get("https://randomuser.me/api/?results=10&nat=us");
   }
 };
